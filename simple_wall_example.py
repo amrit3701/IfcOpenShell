6e07c0d65f71314aa4ebdffbd4d2144035f697c3
Amritpal Singh <amrit3701@gmail.com>
2016-02-27 23:09:35 +0530

    update

--- simple_wall_example.py
+++ simple_wall_example.py
@@ -5,10 +5,10 @@ import ifcopenshell
 import pdb
 
 
-O = 1., 1., 1.
+O = 0., 0., 0.
 X = 1., 0., 0.
-Y = 0., 1., 1.
-Z = 1., 0., 1.
+Y = 0., 1., 0.
+Z = 0., 0., 1.
 
 # Helper function definitions
 
@@ -61,8 +61,8 @@ print("create_guid %s" %create_guid)
 filename = "hello_wall.ifc"
 timestamp = time.time()
 timestring = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(timestamp))
-creator = "Kianwee Chen"
-organization = "ETHZ"
+creator = "Amritpal Singh"
+organization = "AMBU"
 application, application_version = "IfcOpenShell", "0.5"
 project_globalid, project_name = create_guid(), "Hello Wall"
 
@@ -98,8 +98,10 @@ ENDSEC;
 END-ISO-10303-21;
 """ % locals()
 
+
 # Write the template to a temporary file
 temp_handle, temp_filename = tempfile.mkstemp(suffix=".ifc")
+#pdb.set_trace()
 with open(temp_filename, "wb") as f:
     f.write(template)
 
@@ -112,6 +114,8 @@ context = ifcfile.by_type("IfcGeometricRepresentationContext")[0]
 # IFC hierarchy creation
 site_placement = create_ifclocalplacement(ifcfile)
 site = ifcfile.createIfcSite(create_guid(), owner_history, "Site", None, None, site_placement, None, None, "ELEMENT", None, None, None, None, None)
+print("site %s" % site)
+
 
 building_placement = create_ifclocalplacement(ifcfile, relative_to=site_placement)
 building = ifcfile.createIfcBuilding(create_guid(), owner_history, 'Building', None, None, building_placement, None, None, "ELEMENT", None, None, None)
@@ -131,64 +135,96 @@ container_project = ifcfile.createIfcRelAggregates(create_guid(), owner_history,
 # Wall creation: Define the wall shape as a polyline axis and an extruded area solid
 wall_placement = create_ifclocalplacement(ifcfile, relative_to=storey_placement)
 wall_placement = create_ifclocalplacement(ifcfile)
-polyline = create_ifcpolyline(ifcfile, [(10.0, 0.0, 0.0), (5.0, 0.0, 0.0)])
+polyline = create_ifcpolyline(ifcfile, [(0.0, 0.0, 0.0), (5.0, 0.0, 0.0)])
 axis_representation = ifcfile.createIfcShapeRepresentation(context, "Axis", "Curve2D", [polyline])
 
-extrusion_placement = create_ifcaxis2placement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0))
-point_list_extrusion_area = [(0.0, -.1, 0.0), (10.0, -.1, 0.0), (10.0,
-    .1, 0.0), (0.0, .1, 0.0), (0.0, -.1, 0.0)]
+extrusion_placement = create_ifcaxis2placement(ifcfile, (0.0, 0.0, 0.0),
+        (0.0, 0.0, 1.0), (1.0, 0.0, 0.0))
+point_list_extrusion_area = [(0.0, -.2, 0.0), (5.0, -.2, 0.0), (5.0,
+    .2, 0.0), (0.0, .2, 0.0), (0.0, -.2, 0.0)]
 solid = create_ifcextrudedareasolid(ifcfile, point_list_extrusion_area,
         extrusion_placement, (0.0, 0.0, 1.0), 3.0)
+
+print("solidddddddddd %s" % solid.Depth)
 body_representation = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [solid])
 
 product_shape = ifcfile.createIfcProductDefinitionShape(None, None, [axis_representation, body_representation])
 
 wall = ifcfile.createIfcWallStandardCase(create_guid(), owner_history, "Wall", "An awesome wall", None, wall_placement, product_shape, None)
 
+# new wall
+# Wall creation: Define the wall shape as a polyline axis and an extruded area solid
+wall_placement2 = create_ifclocalplacement(ifcfile, relative_to=storey_placement)
+wall_placement2 = create_ifclocalplacement(ifcfile)
+polyline2 = create_ifcpolyline(ifcfile, [(0.0, 0.0, 0.0), (5.0, 0.0, 0.0)])
+axis_representation2 = ifcfile.createIfcShapeRepresentation(context, "Axis", "Curve2D", [polyline2])
+
+extrusion_placement2 = create_ifcaxis2placement(ifcfile, (0.2, 0.2, 0.0),
+        (0.0, 0.0, 1.0), (0.0, 1.0, 0.0))
+point_list_extrusion_area2 = [(0.0, -.2, 0.0), (5.0, -.2, 0.0), (5.0,
+    .2, 0.0), (0.0, .2, 0.0), (0.0, -.2, 0.0)]
+solid2 = create_ifcextrudedareasolid(ifcfile, point_list_extrusion_area2,
+        extrusion_placement2, (0.0, 0.0, 1.0), 3.0)
+
+print("solidddddddddd %s" % solid.Depth)
+body_representation2 = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [solid2])
+
+product_shape2 = ifcfile.createIfcProductDefinitionShape(None, None, [axis_representation2, body_representation2])
+
+wall2 = ifcfile.createIfcWallStandardCase(create_guid(), owner_history, "Wall", "An awesome wall", None, wall_placement2, product_shape2, None)
+
+
 # Define and associate the wall material
-#material = ifcfile.createIfcMaterial("wall material")
-#material_layer = ifcfile.createIfcMaterialLayer(material, 0.2, None)
-#material_layer_set = ifcfile.createIfcMaterialLayerSet([material_layer], None)
-#material_layer_set_usage = ifcfile.createIfcMaterialLayerSetUsage(material_layer_set, "AXIS2", "POSITIVE", -0.1)
-#ifcfile.createIfcRelAssociatesMaterial(create_guid(), owner_history, RelatedObjects=[wall], RelatingMaterial=material_layer_set_usage)
+material = ifcfile.createIfcMaterial("wall material")
+material_layer = ifcfile.createIfcMaterialLayer(material, .2, None)
+material_layer_set = ifcfile.createIfcMaterialLayerSet([material_layer], None)
+material_layer_set_usage = ifcfile.createIfcMaterialLayerSetUsage(material_layer_set, "AXIS2", "POSITIVE", -0.1)
+ifcfile.createIfcRelAssociatesMaterial(create_guid(), owner_history, RelatedObjects=[wall], RelatingMaterial=material_layer_set_usage)
 
 # Create and assign property set
-#property_values = [
-#    ifcfile.createIfcPropertySingleValue("Reference", "Reference", ifcfile.create_entity("IfcText", "Describe the Reference"), None),
-#    ifcfile.createIfcPropertySingleValue("IsExternal", "IsExternal", ifcfile.create_entity("IfcBoolean", True), None),
-#    ifcfile.createIfcPropertySingleValue("ThermalTransmittance", "ThermalTransmittance", ifcfile.create_entity("IfcReal", 2.569), None),
-#    ifcfile.createIfcPropertySingleValue("IntValue", "IntValue", ifcfile.create_entity("IfcInteger", 2), None)
-#]
-#property_set = ifcfile.createIfcPropertySet(create_guid(), owner_history, "Pset_WallCommon", None, property_values)
-#ifcfile.createIfcRelDefinesByProperties(create_guid(), owner_history, None, None, [wall], property_set)
+property_values = [
+    ifcfile.createIfcPropertySingleValue("Reference", "Reference", ifcfile.create_entity("IfcText", "Describe the Reference"), None),
+    ifcfile.createIfcPropertySingleValue("IsExternal", "IsExternal", ifcfile.create_entity("IfcBoolean", True), None),
+    ifcfile.createIfcPropertySingleValue("ThermalTransmittance", "ThermalTransmittance", ifcfile.create_entity("IfcReal", 2.569), None),
+    ifcfile.createIfcPropertySingleValue("IntValue", "IntValue", ifcfile.create_entity("IfcInteger", 2), None)
+]
+property_set = ifcfile.createIfcPropertySet(create_guid(), owner_history, "Pset_WallCommon", None, property_values)
+ifcfile.createIfcRelDefinesByProperties(create_guid(), owner_history, None, None, [wall], property_set)
 
 # Add quantity information
-#quantity_values = [
-#    ifcfile.createIfcQuantityLength("Length", "Length of the wall", None, 5.0),
-#    ifcfile.createIfcQuantityArea("Area", "Area of the front face", None, 5.0 * solid.Depth),
-#    ifcfile.createIfcQuantityVolume("Volume", "Volume of the wall", None, 5.0 * solid.Depth * material_layer.LayerThickness)
-#]
-#element_quantity = ifcfile.createIfcElementQuantity(create_guid(), owner_history, "BaseQuantities", None, None, quantity_values)
-#ifcfile.createIfcRelDefinesByProperties(create_guid(), owner_history, None, None, [wall], element_quantity)
+quantity_values = [
+    ifcfile.createIfcQuantityLength("Length", "Length of the wall", None, 5.0),
+    ifcfile.createIfcQuantityArea("Area", "Area of the front face", None, 5.0 * solid.Depth),
+    ifcfile.createIfcQuantityVolume("Volume", "Volume of the wall", None, 5.0 * solid.Depth * material_layer.LayerThickness)
+]
+print("solid depth %s" %solid.Depth)
+print("material_layer %s" %material_layer.LayerThickness)
+
+element_quantity = ifcfile.createIfcElementQuantity(create_guid(), owner_history, "BaseQuantities", None, None, quantity_values)
+ifcfile.createIfcRelDefinesByProperties(create_guid(), owner_history, None, None, [wall], element_quantity)
 
 # Create and associate an opening for the window in the wall
-#opening_placement = create_ifclocalplacement(ifcfile, (0.5, 0.0, 1.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0), wall_placement)
-#opening_extrusion_placement = create_ifcaxis2placement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0))
-#point_list_opening_extrusion_area = [(0.0, -0.1, 0.0), (3.0, -0.1, 0.0), (3.0, 0.1, 0.0), (0.0, 0.1, 0.0), (0.0, -0.1, 0.0)]
-#opening_solid = create_ifcextrudedareasolid(ifcfile, point_list_opening_extrusion_area, opening_extrusion_placement, (0.0, 0.0, 1.0), 1.0)
-#opening_representation = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [opening_solid])
-#opening_shape = ifcfile.createIfcProductDefinitionShape(None, None, [opening_representation])
-#opening_element = ifcfile.createIfcOpeningElement(create_guid(), owner_history, "Opening", "An awesome opening", None, opening_placement, opening_shape, None)
-#ifcfile.createIfcRelVoidsElement(create_guid(), owner_history, None, None, wall, opening_element)
+opening_placement = create_ifclocalplacement(ifcfile, (.5, 0.0, 1), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0), wall_placement)
+opening_extrusion_placement = create_ifcaxis2placement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0))
+point_list_opening_extrusion_area = [(0.0, -0.2, 0.0), (3.0, -0.2, 0.0), (3.0, 0.2, 0.0), (0.0, 0.2, 0.0), (0.0, -0.2, 0.0)]
+opening_solid = create_ifcextrudedareasolid(ifcfile, point_list_opening_extrusion_area, opening_extrusion_placement, (0.0, 0.0, 1.0), 1.3)
+opening_representation = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [opening_solid])
+opening_shape = ifcfile.createIfcProductDefinitionShape(None, None, [opening_representation])
+opening_element = ifcfile.createIfcOpeningElement(create_guid(), owner_history, "Opening", "An awesome opening", None, opening_placement, opening_shape, None)
+ifcfile.createIfcRelVoidsElement(create_guid(), owner_history, None, None, wall, opening_element)
 
 # Create a simplified representation for the Window
-#window_placement = create_ifclocalplacement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0), opening_placement)
-#window_extrusion_placement = create_ifcaxis2placement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0))
-#point_list_window_extrusion_area = [(0.0, -0.01, 0.0), (3.0, -0.01, 0.0), (3.0, 0.01, 0.0), (0.0, 0.01, 0.0), (0.0, -0.01, 0.0)]
-#window_solid = create_ifcextrudedareasolid(ifcfile, point_list_window_extrusion_area, window_extrusion_placement, (0.0, 0.0, 1.0), 1.0)
-#window_representation = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [window_solid])
+window_placement = create_ifclocalplacement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0), opening_placement)
+window_extrusion_placement = create_ifcaxis2placement(ifcfile, (0.0, 0.0, 0.0), (0.0, 0.0, 1.0), (1.0, 0.0, 0.0))
+point_list_window_extrusion_area = [(0.0, -0.06, 0.0), (3.0, -0.06, 0.0), (3.0, 0.06, 0.0), (0.0, 0.06, 0.0), (0.0, -0.06, 0.0)]
+window_solid = create_ifcextrudedareasolid(ifcfile, point_list_window_extrusion_area, window_extrusion_placement, (0.0, 0.0, 1.0), 1.3)
+window_representation = ifcfile.createIfcShapeRepresentation(context, "Body", "SweptSolid", [window_solid])
+window_shape = ifcfile.createIfcProductDefinitionShape(None, None, [window_representation])
+window = ifcfile.createIfcWindow(create_guid(), owner_history, "Window", "An awesome window", None, window_placement, window_shape, None, None)
+
+
 #window_shape = ifcfile.createIfcProductDefinitionShape(None, None, [window_representation])
-#window = ifcfile.createIfcWindow(create_guid(), owner_history, "Window", "An awesome window", None, window_placement, window_shape, None, None)
+#window = ifcfile.createIfcWallStandardCase(create_guid(), owner_history, "Wall", "An awesome wall", None, window_placement, window_shape, None)
 
 # Relate the window to the opening element
 #ifcfile.createIfcRelFillsElement(create_guid(), owner_history, None, None, opening_element, window)
